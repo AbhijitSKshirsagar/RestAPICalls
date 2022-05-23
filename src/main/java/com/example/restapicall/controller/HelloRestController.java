6b869cef5bd2192e98e83a3f6166a001ce6af92d
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-05-23 14:11:01 +0530

    added: Rest call by passing FisrtName as path variable and LastName as query

--- src/main/java/com/example/restapicall/controller/HelloRestController.java
+++ src/main/java/com/example/restapicall/controller/HelloRestController.java
@@ -25,4 +25,8 @@ public class HelloRestController {
     public String userData(@RequestBody User user) {
         return "Hello "+user.getFirstName()+" "+user.getLastName()+" from BridgeLabz";
     }
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,@RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
 }
